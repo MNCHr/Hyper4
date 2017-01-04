9b48060086f20b2bc2fb9a08325b18913f37b528
David Hancock <dhancock@cs.utah.edu>
2017-01-04 16:09:01 -0700

    Bug fixes. Ready to start spitting out tset_control table entries.

--- hp4c/hp4c.py
+++ hp4c/hp4c.py
@@ -7,6 +7,7 @@ from collections import OrderedDict
 import argparse
 import code
 import sys
+import math
 
 def parse_args(args):
   parser = argparse.ArgumentParser(description='HP4 Compiler')
@@ -36,12 +37,12 @@ class HP4_Command:
 
 class HP4C:
   def __init__(self, h, args):
-    self.parseControlStates = {}
-    self.bits_needed_local = {}
-    self.bits_needed_total = {}
+    self.pc_bits_extracted = {}
+    self.pc_bits_extracted_curr = {}
+    self.pc_action = {}
     self.field_offsets = {}
+    self.offset = 0
     self.commands = []
-    self.pc_state_id = 1
     self.h = h
     self.h.build()
     if len(self.h.p4_ingress_ptr) > 1:
@@ -57,82 +58,110 @@ class HP4C:
                                        "set_program",
                                        ["[PPORT]"],
                                        ["[program ID]", "[VPORT_0]"]))
-
-  def collect_local_bits_needed(self, parse_state):
-    numbits = 0
-    # collect total number of bits to extract in the state
+      
+  def walk_parse_tree(self, parse_state, pc_state):
+    numbits = self.offset
     for call in parse_state.call_sequence:
       if call[0].value != 'extract':
         print("ERROR: unsupported call %s" % call[0].value)
         exit()
-      for key in call[1].header_type.layout.keys():
-        numbits += call[1].header_type.layout[key]
-
-    maxcurr = 0
-    # look for 'current' instances in the return statement
-    if parse_state.return_statement[0] == 'select':
+      # update field_offsets, numbits
+      for field in call[1].fields:
+        self.field_offsets[call[1].name + '.' + field.name] = self.offset
+        self.offset += field.width # advance current offset
+        numbits += field.width
+    self.pc_bits_extracted[pc_state] = numbits 
+  
+    # traverse parse tree
+    next_states = []
+    if parse_state.return_statement[0] == 'immediate':
+      if parse_state.return_statement[1] != 'ingress':
+        next_state = self.h.p4_parse_states[parse_state.return_statement[1]]
+        self.walk_parse_tree(next_state, pc_state)
+      else:
+        self.pc_action[pc_state] = '[PROCEED]'
+        return
+    elif parse_state.return_statement[0] == 'select':
+      # account for 'current' instances
+      maxcurr = 0
+      # identify range of bytes to examine
+      startbytes = 100
+      endbytes = 0
       for criteria in parse_state.return_statement[1]:
         if isinstance(criteria, tuple): # tuple indicates use of 'current'
           # criteria[0]: start offset from current position
           # criteria[1]: width of 'current' call
-          if criteria[0] + criteria[1] > maxcurr:
-            maxcurr = criteria[0] + criteria[1]
-      numbits += maxcurr
-    elif parse_state.return_statement[0] != 'immediate':
-      print("ERROR: Unknown directive in return statement: %s" % parse_state.return_statement[0])
-      exit()
-    self.bits_needed_local[parse_state] = (numbits, maxcurr)
-
-  def collect_total_bits_needed(self, parse_state, preceding_parse_states):
-    numbits = self.bits_needed_local[parse_state][0]
-    for precstate in preceding_parse_states:
-      numbits += self.bits_needed_local[precstate][0]
-      # deduct for instances of 'current':
-      numbits -= self.bits_needed_local[precstate][1]
-    self.bits_needed_total[(parse_state, tuple(preceding_parse_states))] = numbits
-
-    # build queue of parse_states
-    next_states = []
-    if parse_state.return_statement[0] == 'immediate':
-      if parse_state.return_statement[1] != 'ingress':
-        next_states.append(self.h.p4_parse_states[parse_state.return_statement[1]])
-      else:
-        return
-    elif parse_state.return_statement[0] == 'select':
-      for selectopt in parse_state.return_statement[2]:
-        if selectopt[1] != 'ingress':
-          next_states.append(self.h.p4_parse_states[selectopt[1]])
-    else:
-      print("ERROR: Unknown directive in return statement: %s" % parse_state.return_statement[0])
-      exit()
-
-    # recurse for every next state reachable from this state
-    for next_state in next_states:
-      next_preceding_states = list(preceding_parse_states)
-      next_preceding_states.append(parse_state)
-      self.collect_total_bits_needed(next_state, next_preceding_states)
-
-  def collectParseControlStates(self):
-    for parse_state in self.h.p4_parse_states.values():
-      self.collect_local_bits_needed(parse_state)
-    self.collect_total_bits_needed(self.h.p4_parse_states['start'], [])
-    for key in self.bits_needed_total.keys():
-      if key[0] == self.h.p4_parse_states['start']:
-        self.parseControlStates[key] = 0
-      else:
-        self.parseControlStates[key] = self.pc_state_id
-        self.pc_state_id += 1
+          maxcurr = criteria[0] + criteria[1]
+          if (startbytes * 8) > self.offset + criteria[0]:
+            startbytes = (self.offset + criteria[0]) / 8
+          if (endbytes * 8) < self.offset + maxcurr:
+            endbytes = int(math.ceil((self.offset + maxcurr) / 8.0))
+        else: # single field
+          fieldstart = self.field_offsets[criteria] / 8
+          fieldend = int(math.ceil((fieldstart * 8
+                                    + self.h.p4_fields[criteria].width) / 8.0))
+          if startbytes > fieldstart:
+            startbytes = fieldstart
+          if endbytes < fieldend:
+            endbytes = fieldend
+          
+      self.pc_bits_extracted[pc_state] += maxcurr
+      self.pc_bits_extracted_curr[pc_state] = maxcurr
   
-  #def gen_tset_control_entries(self):
-    #for pc_state in self.parseControlStates.keys():
-      
+      # pc_action[pc_state] = 'inspect_XX_YY'
+      if startbytes >= endbytes:
+        print("ERROR: startbytes(%i) > endbytes(%i)" % (startbytes, endbytes))
+        print("parse_state: %s" % parse_state)
+        exit()
+      def unsupported(sbytes, ebytes):
+        print("Not yet supported: startbytes(%i) and endbytes(%i) cross boundaries"  % (sbytes, ebytes))
+        exit()
+      if startbytes < self.args.seb:
+        if endbytes >= self.args.seb:
+          unsupported(startbytes, endbytes)
+        else:
+          self.pc_action[pc_state] = '[INSPECT_SEB]'
+      else:
+        bound = self.args.seb + 10
+        while bound <= 100:
+          if startbytes < bound:
+            if endbytes >= bound:
+              unsupported(startbytes, endbytes)
+            else:
+              self.pc_action[pc_state] = '[INSPECT_' + str(bound - 10) + '_' \
+                                    + str(bound - 1) + ']'
+              break
+          bound += 10
+      if pc_state not in self.pc_action:
+        print("ERROR: did not find inspect_XX_YY function for startbytes(%i) and endbytes(%i)" % (startbytes, endbytes))
+        exit()
+    
+      for selectopt in parse_state.return_statement[2]:
+        # selectopt: (list of values, next parse_state)
+        if selectopt[1] != 'ingress':
+          next_state = self.h.p4_parse_states[selectopt[1]]
+          if next_state not in next_states:
+            next_states.append(next_state)
+    else:
+      print("ERROR: Unknown directive in return statement: %s" \
+            % parse_state.return_statement[0])
+      exit()
+
+    save_offset = self.offset
+    for next_state in next_states:
+      pc_state += 1
+      self.pc_bits_extracted[pc_state] = save_offset
+      self.walk_parse_tree(next_state, pc_state)
+      self.offset = save_offset
+
+  def gen_tset_control_entries(self):
+    self.walk_parse_tree(self.h.p4_parse_states['start'], 0)
 
 def main():
   args = parse_args(sys.argv[1:])
   hp4c = HP4C(HLIR(args.input), args)
   hp4c.gen_tset_context_entry()
-  hp4c.collectParseControlStates()
-  #hp4c.gen_tset_control_entries()
+  hp4c.gen_tset_control_entries()
   code.interact(local=locals())
 
 if __name__ == '__main__':
