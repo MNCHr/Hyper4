6be021d079f56579d4b5bd30d79e2359e4e51dd3
David Hancock <dhancock@cs.utah.edu>
2017-01-04 15:18:11 -0700

    Complete gen_tset_control_entries method.  Ready for testing.

--- hp4c/gen_tset_control_entries.py
+++ hp4c/gen_tset_control_entries.py
@@ -8,10 +8,8 @@ pc_action = {}
 field_offsets = {}
 offset = 0 # in bits
 
-# TODO: Ensure offset is being moved back upon return from recursive calls from select statements
-
 def gen_tset_control_entries(self, parse_state, pc_state):
-  numbits = 0
+  numbits = self.offset
   for call in parse_state.call_sequence:
     if call[0].value != 'extract':
       print("ERROR: unsupported call %s" % call[0].value)
@@ -33,7 +31,7 @@ def gen_tset_control_entries(self, parse_state, pc_state):
       next_state = self.h.p4_parse_states[parse_state.return_statement[1]]
       gen_tset_control_entries(next_state, pc_state)
     else:
-      pc_action[pc_state] = 'proceed'
+      pc_action[pc_state] = '[PROCEED]'
       return
   elif parse_state.return_statement[0] == 'select':
     # account for 'current' instances
@@ -46,10 +44,10 @@ def gen_tset_control_entries(self, parse_state, pc_state):
         # criteria[0]: start offset from current position
         # criteria[1]: width of 'current' call
         maxcurr = criteria[0] + criteria[1]
-        if (startbytes * 8) > offset + criteria[0]:
-          startbytes = (offset + criteria[0]) / 8
-        if (endbytes * 8) < offset + maxcurr:
-          endbytes = int(math.ceil((offset + maxcurr) / 8.0))
+        if (startbytes * 8) > self.offset + criteria[0]:
+          startbytes = (self.offset + criteria[0]) / 8
+        if (endbytes * 8) < self.offset + maxcurr:
+          endbytes = int(math.ceil((self.offset + maxcurr) / 8.0))
       else: # single field
         fieldstart = field_offsets[criteria] / 8
         fieldend = int(math.ceil((fieldstart
@@ -73,7 +71,7 @@ def gen_tset_control_entries(self, parse_state, pc_state):
       if endbytes >= self.args.seb:
         unsupported(startbytes, endbytes)
       else:
-        pc_action[pc_state] = 'inspect_SEB'
+        pc_action[pc_state] = '[INSPECT_SEB]'
     else:
       bound = self.args.seb + 10
       while bound <= 100:
@@ -81,27 +79,30 @@ def gen_tset_control_entries(self, parse_state, pc_state):
           if endbytes >= bound:
             unsupported(startbytes, endbytes)
           else:
-            pc_action[pc_state] = 'inspect_' + str(bound - 10) + '_'
-                                  + str(bound - 1)
+            pc_action[pc_state] = '[INSPECT_' + str(bound - 10) + '_'
+                                  + str(bound - 1) + ']'
             break
         bound += 10
     if pc_state not in pc_action:
       print("ERROR: did not find inspect_XX_YY function for startbytes(%i) and endbytes(%i)" % (startbytes, endbytes))
+      exit()
     
     for selectopt in parse_state.return_statement[2]:
       # selectopt: (list of values, next parse_state)
       if selectopt[1] != 'ingress':
-        next_states.append(self.h.p4_parse_states[selectopt[1]])
+        next_state = self.h.p4_parse_states[selectopt[1]]
+        if next_state not in next_states:
+          next_states.append(next_state)
   else:
     print("ERROR: Unknown directive in return statement: %s" \
           % parse_state.return_statement[0])
     exit()
 
-  prev_state = pc_state
+  save_offset = self.offset
   for next_state in next_states:
     pc_state += 1
-    pc_bits_extracted[pc_state] = (pc_bits_extracted[prev_state]
-                                  - pc_bits_extracted_curr[prev_state])
+    pc_bits_extracted[pc_state] = save_offset
     gen_tset_control_entries(next_state, pc_state)
+    self.offset = save_offset
 
 gen_tset_control_entries(self.h.p4_parse_states['start'], 0)
