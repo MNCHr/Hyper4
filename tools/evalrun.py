f9a22a6b2a927e5801b15bd5dbf9055a8f5726a6
David Hancock <dhancock@cs.utah.edu>
2017-02-14 14:52:33 -0700

    Modify evalrun to produce PDF, CDF, percentiles charts
    This is to be used with pcap files converted to text files
    via tcpdump -r

--- tools/evalrun.py
+++ tools/evalrun.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
-# Takes two pcap files, extracts timestamps, and calculates the average and
-# standard deviation of the differences between timestamps
+# Takes two pcap files, extracts timestamps, and calculates and plots the
+# histogram and CDF of the differences between timestamps
 #
 # Assumption is that file 1 includes timestamps of packets received on the inbound
 # switch interface; file 2 includes timestamps of the corresponding packets sent
@@ -14,7 +14,8 @@ import sys
 import os
 import datetime
 import time
-import numpy
+import numpy as np
+import matplotlib.pyplot as plt
 
 # get run#
 if len(sys.argv) < 2:
@@ -55,10 +56,29 @@ with open(file2txt, 'r') as f2:
 
 # - calculate difference between corresponding timestamps
 for t1, t2 in zip(times1, times2):
-  diffs.append( round( (t2 - t1) * 1000, 3) )
+  diffs.append( abs(round( (t2 - t1) * 1000, 3)) )
 
-# - calculate average, stddev and print to stdout
-arr = numpy.array(diffs)
-#for num in diffs:
-#  print(num)
-print("avg: %.3fms, std: %.3f, count: %i" % (numpy.mean(arr), numpy.std(arr), int(len(diffs)) ))
+arr = np.array(diffs)
+
+density, bins = np.histogram(arr, density=True)
+unity_density = density / density.sum()
+pbins = np.arange(0, 104, 4)
+#pbins = np.array([0, 25, 50, 90, 100])
+pindices = np.arange(len(pbins))
+percentile = np.zeros(len(pbins))
+for i in range(len(pbins)):
+  percentile[i] = np.percentile(arr, pbins[i])
+
+fig, (ax1, ax2, ax3) = plt.subplots(nrows=1, ncols=3, sharex=False, figsize=(12,4))
+widths = bins[:-1] - bins[1:]
+ax1.bar(bins[1:], unity_density, width=widths)
+ax2.bar(bins[1:], unity_density.cumsum(), width = widths)
+#ax3.plot(pindices, percentile)
+ax3.plot(pbins, percentile)
+#ax3.xaxis.set_ticks(pindices)
+#ax3.xaxis.set_ticklabels(pbins)
+ax1.set_xlabel('PDF')
+ax2.set_xlabel('CDF')
+ax3.set_xlabel('Percentiles')
+fig.tight_layout()
+plt.show()
