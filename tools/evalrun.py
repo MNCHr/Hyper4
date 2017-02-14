a4296e33644b72b11232dd5c0a993cea1c785c6f
David Hancock <dhancock@cs.utah.edu>
2017-02-14 15:59:06 -0700

    Finish evalrun mods, should be much better tool now
    This whole thing was prompted by a slide deck I saw making the
    strong argument that especially when measuring latency, means
    and standard deviations are not the right way to evaluate a
    system.  CDFs and percentile charts are much better (though
    they must also be used with care).

--- tools/evalrun.py
+++ tools/evalrun.py
@@ -3,9 +3,12 @@
 # Takes two pcap files, extracts timestamps, and calculates and plots the
 # histogram and CDF of the differences between timestamps
 #
-# Assumption is that file 1 includes timestamps of packets received on the inbound
+# Assumption 1 is that file 1 includes timestamps of packets received on the inbound
 # switch interface; file 2 includes timestamps of the corresponding packets sent
 # out the outbound switch interface.
+#
+# Assumption 2 is there are at least 1000 samples.  If less, recommend adjusting the
+# percentile values in pbins.
 #
 # DAVID HANCOCK
 # University of Utah
@@ -16,6 +19,7 @@ import datetime
 import time
 import numpy as np
 import matplotlib.pyplot as plt
+import code
 
 # get run#
 if len(sys.argv) < 2:
@@ -37,48 +41,74 @@ execstr2 = "tcpdump -r " + file2pcap + " > " + file2txt
 os.system(execstr1)
 os.system(execstr2)
 
-times1 = []
-times2 = []
+times1 = {}
+times2 = {}
 diffs = []
 
+curr_year = int(time.strftime("%Y"))
+curr_month = int(time.strftime("%m"))
+curr_day = int(time.strftime("%d"))
+
 # process the two files
+i = 0
 with open(file1txt, 'r') as f1:
   for line in f1:
+    i += 1
     # store the time in an array
     t = datetime.datetime.strptime( line.split()[0], "%H:%M:%S.%f" )
-    times1.append( time.mktime(t.timetuple()) + (t.microsecond / 1000000.0) )
-    
+    t = t.replace(year=curr_year, month=curr_month, day=curr_day)
+    ip = line.split(' ')[2]
+    seq = int(line.split(', ')[2].split('seq ')[1])
+    times1[(ip, seq)] = time.mktime(t.timetuple()) + (t.microsecond / 1000000.0)
+
+if i < 1000:
+  print("WARNING: number of samples in " + file1txt + " (%d) less than 1000; percentile values may not be appropriate" % i)
+
+j = 0
 with open(file2txt, 'r') as f2:
+  j += 1
   for line in f2:
     # store the time in an array
     t = datetime.datetime.strptime( line.split()[0], "%H:%M:%S.%f" )
-    times2.append( time.mktime(t.timetuple()) + (t.microsecond / 1000000.0) )
+    t = t.replace(year=curr_year, month=curr_month, day=curr_day)
+    ip = line.split(' ')[2]
+    seq = int(line.split(', ')[2].split('seq ')[1])
+    times2[(ip, seq)] = time.mktime(t.timetuple()) + (t.microsecond / 1000000.0)
+
+if j < 1000:
+  print("WARNING: number of samples in " + file2txt + " (%d) less than 1000; percentile values may not be appropriate" % j)
+
+if i != j:
+  print("WARNING: " + file1txt + " and " + file2txt + " do not have the same number of samples")
 
 # - calculate difference between corresponding timestamps
-for t1, t2 in zip(times1, times2):
-  diffs.append( abs(round( (t2 - t1) * 1000, 3)) )
+for key in times1.keys():
+  diffs.append( abs(round( (times2[key] - times1[key]) * 1000, 3)) )
 
 arr = np.array(diffs)
 
 density, bins = np.histogram(arr, density=True)
 unity_density = density / density.sum()
-pbins = np.arange(0, 104, 4)
-#pbins = np.array([0, 25, 50, 90, 100])
+#pbins = np.arange(0, 104, 4)
+pbins = np.array([0, 50, 90, 99, 99.9, 100])
 pindices = np.arange(len(pbins))
 percentile = np.zeros(len(pbins))
 for i in range(len(pbins)):
   percentile[i] = np.percentile(arr, pbins[i])
 
+code.interact(local=locals())
+
 fig, (ax1, ax2, ax3) = plt.subplots(nrows=1, ncols=3, sharex=False, figsize=(12,4))
 widths = bins[:-1] - bins[1:]
 ax1.bar(bins[1:], unity_density, width=widths)
 ax2.bar(bins[1:], unity_density.cumsum(), width = widths)
-#ax3.plot(pindices, percentile)
-ax3.plot(pbins, percentile)
-#ax3.xaxis.set_ticks(pindices)
-#ax3.xaxis.set_ticklabels(pbins)
-ax1.set_xlabel('PDF')
-ax2.set_xlabel('CDF')
+ax3.plot(pindices, percentile)
+#ax3.plot(pbins, percentile)
+ax3.xaxis.set_ticks(pindices)
+ax3.xaxis.set_ticklabels(pbins)
+ax1.set_xlabel('PDF (ms)')
+ax2.set_xlabel('CDF (ms)')
 ax3.set_xlabel('Percentiles')
+ax3.set_ylabel('milliseconds')
 fig.tight_layout()
 plt.show()
